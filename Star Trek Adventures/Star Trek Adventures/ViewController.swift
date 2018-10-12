dfbf9add92958541251d6b1e3a26c06b3d050446
TheMartianLife <github@mail.themartianlife.com>
2018-10-12 22:52:03 +1100

    Initial commit

---
+++ Star Trek Adventures/Star Trek Adventures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Trek Adventures
+//
+//  Created by Mars Geldard on 12/10/18.
+//  Copyright © 2018 Mars Geldard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
